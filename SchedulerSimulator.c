f77973443abd2bdc08c16bd071542a61eaf87684
woosukqw12 <woosukqw@korea.ac.kr>
2025-04-30 23:21:43 +0900

    add the structure of process

---
+++ SchedulerSimulator.c
@@ -0,0 +1,14 @@
+
+
+typedef struct{
+    int PID;
+    int arrival_time;
+    int CPU_burst_time;
+    float IO_burst_time;
+    float IO_request_time;
+    int priority;
+} process;
+
+void Create_Process(void){
+
+}
