22efc2a2d02000e2492e5167e11f2a616449a266
woosuqw12 <woosukqw@korea.ac.kr>
2025-05-27 00:03:30 +0900

    오타 수정

--- SchedulerSimulator.c
+++ SchedulerSimulator.c
@@ -7,8 +7,8 @@
 
 //global assign 
 
-char *algorithms[NUMofAlgorithms] = {"FCFS", "Non-preempitve SJF", "Non-preempitve Priority", "RR", \
-    "Preemptive SJF", "Preemptive Priority", "HRN", "Non-preempitve Custom", "preempitve Custom", "lottery"};
+char *algorithms[NUMofAlgorithms] = {"FCFS", "Non-preemptive SJF", "Non-preemptive Priority", "RR", \
+    "Preemptive SJF", "Preemptive Priority", "HRN", "Non-preempitve Custom", "preemptive Custom", "lottery"};
 evaluation_result* result_array;
 //function assign
 // void Schedule(void);
